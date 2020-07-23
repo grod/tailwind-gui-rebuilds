b38ef8702377e23ce605a50d4f5057fe001588bf
Gustav B <gustav.buchholtz@gmail.com>
2020-07-23 20:08:37 +0200

    Hover states

--- src/pages/netlify/_layout.svelte
+++ src/pages/netlify/_layout.svelte
@@ -15,9 +15,13 @@
                     </div>
                 </div>
             </div>
-            <div class="flex items-center space-x-6 text-gray-600">
-                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zm0 16a3 3 0 01-3-3h6a3 3 0 01-3 3z"/></svg>
-                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"/></svg>
+            <div class="flex items-center space-x-3 text-gray-600">
+                <button class="px-2 py-1 rounded hover:bg-opacity-25 hover:text-gray-200 hover:bg-black transition duration-150">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zm0 16a3 3 0 01-3-3h6a3 3 0 01-3 3z"/></svg>
+                </button>
+                <button class="px-2 py-1 rounded hover:bg-opacity-25 hover:text-gray-200 hover:bg-black transition duration-150">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"/></svg>
+                </button>
                 <img class="w-10 h-10 border-2 border-gray-800 hover:border-gray-100 rounded-full object-cover" src="grod-avatar.png" alt="Avatar" />
             </div>
         </div>
