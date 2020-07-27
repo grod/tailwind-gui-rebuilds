6854943a89213142ea8e24dbe2cba8773a2e7f74
Gustav B <gustav.buchholtz@gmail.com>
2020-07-27 21:06:16 +0200

    Added prismic

---
+++ src/pages/prismic/index.svelte
@@ -0,0 +1,154 @@
+<div class="fixed p-3 bottom-0 right-0 mr-4 mb-4 rounded-full shadow-lg bg-indigo-600 text-white">
+    <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd"></path></svg>
+</div>
+
+<div class="flex flex-col h-screen">
+    <div class="flex">
+        <div class="flex items-center justify-center w-16 h-16 bg-indigo-700">
+            <div class="inline-block p-2 rounded-full bg-green-300">
+                <img class="w-5 h-5" src="prismic-logo.svg" alt="">
+            </div>
+        </div>
+        <div class="flex flex-1 items-center px-10 space-x-10 font-medium text-sm text-gray-100 bg-indigo-600">
+            <a href="/" class="flex items-center h-full pt-1 border-b-4 border-indigo-300">
+                <span>Work</span>
+            </a>
+            <a href="/" class="flex items-center h-full pt-1 border-b-4 border-transparent">
+                <span>Planned</span>
+                <svg class="mt-1 ml-2 w-3 h-3" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+            </a>
+            <a href="/" class="flex items-center h-full pt-1 border-b-4 border-transparent">
+                <span>Archived</span>
+            </a>
+        </div>
+    </div>
+
+    <div class="flex-1 flex">
+        <div class="flex flex-col justify-between items-center w-16 py-4 border-r border-gray-300 bg-gray-200">
+            <div class="flex flex-col space-y-6">
+                <button class="p-1 border rounded-sm shadow-sm text-gray-600 border-gray-400 bg-gray-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 6h16M4 12h16M4 18h7"></path></svg>
+                </button>
+                <button class="p-1 border rounded-sm text-gray-600 border-transparent">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"></path></svg>
+                </button>
+                <button class="p-1 border rounded-sm text-gray-600 border-transparent">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path></svg>
+                </button>
+                <button class="p-1 border rounded-sm text-gray-600 border-transparent">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path></svg>
+                </button>
+            </div>
+            <div class="flex flex-col space-y-6">
+                <button class="p-1 border rounded-sm text-gray-600 border-transparent">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                </button>
+                <button class="p-1 border rounded-sm text-gray-600 border-transparent">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
+                </button>
+            </div>
+        </div>
+        <div class="flex-1 px-10 py-6 bg-gray-100">
+            <div>
+                <div class="flex justify-between">
+                    <div class="flex space-x-8 text-sm text-gray-600">
+                        <button class="font-light">
+                            <span>Type</span>
+                            <svg class="inline-block ml-px w-4 h-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+                        </button>
+                        <button class="font-light">
+                            <span>Authors</span>
+                            <svg class="inline-block ml-px w-4 h-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+                        </button>
+                        <button class="font-light">
+                            <span>Translations</span>
+                            <svg class="inline-block ml-px w-4 h-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+                        </button>
+                    </div>
+                    <div class="flex space-x-3">
+                        <input class="px-3 py-2 border rounded shadow-inner font-light text-sm border-gray-400 focus:outline-none focus:border-gray-600" placeholder="Search" />
+                        <button class="flex items-center px-3 py-2 rounded shadow border border-gray-400 bg-white hover:bg-gray-100">
+                            <img class="inline-block h-4" src="se.png" alt="" />
+                            <svg class="inline-block ml-1 w-4 h-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+                        </button>
+                        <button class="px-2 py-2 rounded shadow border text-white border-green-600 bg-green-500 hover:bg-green-600 hover:border-green-700">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full mt-4 border rounded text-left text-sm border-gray-300 bg-white">
+                <div class="flex justify-between px-4 py-2 font-medium">
+                    <div class="w-32">
+                        State
+                    </div>
+                    <div class="flex-1">
+                        Name
+                    </div>
+                    <div class="w-32">
+                        Type
+                    </div>
+                    <div class="w-32">
+                        Last update
+                    </div>
+                    <div class="w-10 truncate">
+                        Auth..
+                    </div>
+                </div>
+                <a href="/" class="flex items-center justify-between px-4 py-4 font-light border-t border-gray-300 hover:bg-gray-100">
+                    <div class="w-32">
+                        <div class="inline-block px-2 py-px rounded-sm text-xs text-green-800 bg-green-300">Live</div>
+                    </div>
+                    <div class="flex-1 font-medium">
+                        Home
+                    </div>
+                    <div class="w-32">
+                        Homepage
+                    </div>
+                    <div class="w-32">
+                        6 days ago
+                    </div>
+                    <div class="w-10 truncate">
+                        <img class="w-8 h-8 rounded-full" src="grod-avatar.png" alt="" />
+                    </div>
+                </a>
+                <a href="/" class="flex items-center justify-between px-4 py-4 font-light border-t border-gray-300 hover:bg-gray-100">
+                    <div class="w-32">
+                        <div class="inline-block px-2 py-px rounded-sm text-xs text-green-800 bg-green-300">Live</div>
+                    </div>
+                    <div class="flex-1 font-medium">
+                        About
+                    </div>
+                    <div class="w-32">
+                        Default page
+                    </div>
+                    <div class="w-32">
+                        4 days ago
+                    </div>
+                    <div class="w-10 truncate">
+                        <img class="w-8 h-8 rounded-full" src="grod-avatar.png" alt="" />
+                    </div>
+                </a>
+                <a href="/" class="flex items-center justify-between px-4 py-4 font-light border-t border-gray-300 hover:bg-gray-100">
+                    <div class="w-32">
+                        <div class="inline-block px-2 py-px rounded-sm text-xs text-yellow-800 bg-yellow-300">Draft</div>
+                    </div>
+                    <div class="flex-1 font-medium">
+                        Lorem ipsum dolor sit amet
+                    </div>
+                    <div class="w-32">
+                        Blog post
+                    </div>
+                    <div class="w-32">
+                        6 days ago
+                    </div>
+                    <div class="w-10 truncate">
+                        <img class="w-8 h-8 rounded-full" src="grod-avatar.png" alt="" />
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+</div>
