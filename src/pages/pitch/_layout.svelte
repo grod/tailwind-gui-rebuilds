9b67ecd28f1e7f80a08821a77525970975b9321f
Gustav B <gustav.buchholtz@gmail.com>
2020-07-27 18:37:26 +0200

    Added Pitch

---
+++ src/pages/pitch/_layout.svelte
@@ -0,0 +1,58 @@
+<div class="flex flex-col h-screen text-gray-700">
+    <div class="flex flex-col shadow z-10">
+        <header class="flex items-center border-b text-xs bg-gray-100">
+            <button class="px-5 hover:text-gray-800 transition duration-100">
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+            </button>
+            <button href="/" class="px-5 py-2 -mb-px border-l border-r bg-white">Company updates</button>
+            <button href="/" class="px-5 py-2 -mb-px text-gray-500 hover:text-gray-600 transition duration-100">Weekly Team Bulletin</button>
+            <button class="px-4 border-l border-gray-300 text-gray-500 hover:text-gray-600 transition duration-100">
+                <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 4v16m8-8H4"></path></svg>
+            </button>
+        </header>
+
+        <div class="flex justify-between px-10 py-1">
+            <button class="group block text-left">
+                <div class="flex items-center font-medium text-sm group-hover:text-gray-600 transition duration-100">
+                    <span>Company update</span>
+                    <svg class="w-3 h-3 ml-1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7"></path></svg>
+                </div>
+                <div class="text-xs text-gray-500">Board Reports</div>
+            </button>
+            <div class="flex space-x-5 text-gray-500">
+                <button class="hover:text-gray-600 transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                </button>
+                <button class="hover:text-gray-600 transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
+                </button>
+                <button class="hover:text-gray-600 transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path><path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path></svg>
+                </button>
+                <button class="hover:text-gray-600 transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"></path></svg>
+                </button>
+                <button class="hover:text-gray-600 transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path></svg>
+                </button>
+                <button class="hover:text-gray-600 transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"></path></svg>
+                </button>
+                <button class="hover:text-gray-600 transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path></svg>
+                </button>
+            </div>
+
+            <div class="flex items-center flex-row-reverse">
+                <a class="ml-3 text-xs text-blue-700 hover:underline" href="/">Share</a>
+                <img class="w-8 h-8 -ml-3 object-cover rounded-full border-2 border-white" src="https://images.unsplash.com/photo-1570045006801-08ec03621a42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80" alt="">
+                <img class="w-8 h-8 -ml-3 object-cover rounded-full border-2 border-white" src="https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="">
+                <img class="w-8 h-8 object-cover rounded-full border-2 border-white" src="https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="flex flex-1 bg-gray-100">
+        <slot />
+    </div>
+</div>
---
+++ src/pages/pitch/index.svelte
@@ -0,0 +1,33 @@
+<div class="flex-1 flex">
+   <div class="py-6 pr-6 space-y-6 border-r">
+       <div class="relative flex items-center border-l-2 text-xs border-blue-700">
+           <span class="w-8 font-medium text-center text-blue-600">1</span>
+           <img class="w-32 border rounded-sm border-blue-700" src="screenshot.png" alt="" />
+           <button class="absolute flex items-center justify-center top-0 right-0 w-5 h-5 -mt-2 -mr-2 rounded-full border border-gray-200 bg-green-400 hover:bg-green-300 transition duration-100">
+               <svg class="w-3 h-3 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M5 13l4 4L19 7"></path></svg>
+           </button>
+       </div>
+       <button class="relative flex items-center border-l-2 text-xs border-transparent">
+           <span class="w-8 font-medium text-center text-gray-600">2</span>
+           <img class="w-32 border rounded-sm border-gray-400 hover:border-gray-500 transition duration-100" src="screenshot.png" alt="" />
+           <button class="absolute flex items-center justify-center top-0 right-0 w-5 h-5 -mt-2 -mr-2 rounded-full border border-gray-200 bg-orange-400 hover:bg-orange-300 transition duration-100">
+                <svg class="w-4 h-4 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+           </button>
+       </button>
+       <button class="relative flex items-center border-l-2 text-xs border-transparent">
+           <span class="w-8 font-medium text-center text-gray-600">3</span>
+           <img class="w-32 border rounded-sm border-gray-400 hover:border-gray-500 transition duration-100" src="screenshot.png" alt="" />
+       </button>
+       <button class="group relative flex items-center border-l-2 text-xs border-transparent">
+           <span class="w-8 font-medium text-gray-600"></span>
+           <div class="flex flex-col items-center justify-center w-32 h-20 border rounded-sm text-blue-600 border-gray-400 group-hover:text-blue-800 hover:border-gray-500 transition duration-100">
+                <svg class="w-4 h-4" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 4v16m8-8H4"></path></svg>
+                <div class="mt-px">Add slide</div>
+           </div>
+       </button>
+   </div> 
+   <div class="flex flex-1 p-6">
+       <div class="flex-1 border rounded-sm border-gray-300 bg-white">
+       </div>
+   </div>
+</div>
