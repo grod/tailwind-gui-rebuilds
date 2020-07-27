975bdb86d6758576b5a68b4adb8df570d15c4ebe
Gustav B <gustav.buchholtz@gmail.com>
2020-07-27 22:23:33 +0200

    Buttons

--- src/pages/pitch/index.svelte
+++ src/pages/pitch/index.svelte
@@ -26,8 +26,41 @@
            </div>
        </button>
    </div> 
-   <div class="flex flex-1 p-6">
+   <div class="flex flex-1 p-6 pr-0">
        <div class="flex-1 border rounded-sm border-gray-300 bg-white">
+       </div>
+       <div class="flex flex-col items-center px-4 space-y-6">
+            <div class="text-center">
+                <button class="rounded-full p-2 border shadow border-gray-300 bg-white hover:shadow-md transition duration-100">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z" clip-rule="evenodd"></path></svg>
+                </button>
+                <div class="mt-1 text-xs text-gray-600">Design</div>
+            </div>
+
+            <div class="text-center">
+                <button class="rounded-full p-2 border shadow border-gray-300 bg-white hover:shadow-md transition duration-100">
+                    <div class="flex items-center justify-center top-0 right-0 w-6 h-6 rounded-full bg-green-400 hover:bg-green-300 transition duration-100">
+                        <svg class="w-4 h-4 text-white" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M5 13l4 4L19 7"></path></svg>
+                    </div>
+                    <img class="w-6 h-6 mt-4 rounded-full object-cover" src="https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="" />
+                </button>
+                <div class="mt-1 text-xs text-gray-600">Workflow</div>
+            </div>
+
+            <div class="relative text-center">
+                <button class="rounded-full p-2 border shadow border-gray-300 bg-white hover:shadow-md transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
+                </button>
+                <div class="absolute inline-block px-2 top-0 right-0 -my-1 rounded text-xs text-white bg-blue-600">3</div>
+                <div class="mt-1 text-xs text-gray-600">Comments</div>
+            </div>
+
+            <div class="text-center">
+                <button class="rounded-full p-2 border shadow border-gray-300 bg-white hover:shadow-md transition duration-100">
+                    <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                </button>
+                <div class="mt-1 text-xs text-gray-600">Reaction</div>
+            </div>
        </div>
    </div>
 </div>
